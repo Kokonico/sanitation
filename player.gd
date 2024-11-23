5da597cd0404e92a5f8ce7ecd54a5a47d4d18a05
Kokonico <kokonico@duck.com>
2024-11-23 13:42:00 -0800

    fixed movement fr this time trust

--- player.gd
+++ player.gd
@@ -6,16 +6,20 @@ const SPEED = 1000.0
 const SPEED_CAP = 1000.0
 const FRACTION_FORCE = 100
 var directions = []
-var direction = 0
+var direction = Vector2.ZERO
 var sliding = false
+var has_y_input = false
+var has_x_input = false
 
 var last_direction = Vector2.ZERO
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 	var has_input = false
+	has_y_input = false
+	has_x_input = false
 	
-	if abs(velocity.x) == 0 and abs(velocity.y) == 0:
+	if abs(velocity.x) <= SPEED_CAP and abs(velocity.y) <= SPEED_CAP:
 		sliding = false
 		collision_shape_2d.disabled = false
 	elif sliding == true:
@@ -24,28 +28,36 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move-up"):
 		direction.y -= 1
 		has_input = true
+		has_y_input = true
 	if Input.is_action_pressed("move-down"):
 		direction.y += 1
 		has_input = true
+		has_y_input = true
 	if Input.is_action_pressed("move-left"):
 		direction.x -= 1
 		has_input = true
+		has_x_input = true
 	if Input.is_action_pressed("move-right"):
 		direction.x += 1
 		has_input = true
+		has_x_input = true
 	
-	if has_input and not sliding:
-		direction = direction.normalized()
-		self.rotation = direction.angle()
+	last_direction = direction
+	
+	direction = direction.normalized()
+	self.rotation = direction.angle()
+	if not sliding:
 		velocity += direction * SPEED
 		if abs(velocity.x) > SPEED_CAP:
 			velocity.x = SPEED_CAP * (1 if abs(velocity.x) == velocity.x else -1)
 		if abs(velocity.y) > SPEED_CAP:
 			velocity.y = SPEED_CAP * (1 if abs(velocity.y) == velocity.y else -1)
-	else:
+	
+	if not has_x_input or sliding:
 		var velx_abs = abs(velocity.x)
 		var velx_dir = 1 if velx_abs == velocity.x else -1
 		velocity.x = ((velx_abs - FRACTION_FORCE) if (velx_abs - FRACTION_FORCE) > 0 else 0) * velx_dir
+	if not has_y_input or sliding:
 		var vely_abs = abs(velocity.y)
 		var vely_dir = 1 if vely_abs == velocity.y else -1
 		velocity.y = ((vely_abs - FRACTION_FORCE) if (vely_abs - FRACTION_FORCE) > 0 else 0) * vely_dir
