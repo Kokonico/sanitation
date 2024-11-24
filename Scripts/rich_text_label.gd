a9622e58e8847063417503679095f36a9d638425
Dozez <113320804+Mike-Duz@users.noreply.github.com>
2024-11-24 14:39:27 -0800

    wasd

--- Scripts/rich_text_label.gd
+++ Scripts/rich_text_label.gd
@@ -12,5 +12,12 @@ func _process(delta: float) -> void:
 	if get_parent().get_parent().get_parent().position.y < y:
 		visible_characters += 1
 		if visible_ratio == 1:
-			await get_tree().create_timer(3).timeout
+			if self.name == "RichTextLabel5":
+				get_parent().get_parent().get_parent().get_parent().get_node("Guy1-profile").position = Vector2(11600, -9500)
+				get_parent().get_parent().get_parent().position = Vector2(11600, -9750)
+				get_parent().get_parent().get_parent().hurt(1000)
+				get_parent().get_parent().get_parent().velocity = Vector2.ZERO
+				await get_tree().create_timer(5).timeout
+				get_parent().get_parent().get_parent().get_child(2).get_child(2).visible = true
+			await get_tree().create_timer(1).timeout
 			visible = false
