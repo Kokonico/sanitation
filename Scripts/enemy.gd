4a103a9ed557a1c0ad3d9c572ff04a8402518532
Kokonico <kokonico@duck.com>
2024-11-23 22:45:38 -0800

    gobby improvements

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,18 +1,24 @@
-extends Area2D
+extends CharacterBody2D
+@onready var attack_zone: CollisionShape2D = $AttackRange/CollisionShape2D
+@onready var pathfinder: NavigationAgent2D = $Pathfinder
 
 var following
 var target
+var in_attack_radius
 @export var health = 100
-@export var SPEED = 10
+@export var SPEED = 500
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if following:
+func _physics_process(delta: float) -> void:
+	if following and not in_attack_radius:
 		follow(target)
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -22,9 +28,17 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	
 func follow(target) -> void:
 	var direction = Vector2(target.position-position).normalized()
-	position += direction * SPEED
+	velocity = direction * SPEED
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.get("IS_PLAYER"):
 		following = false
+
+
+func _on_attack_range_body_entered(body: Node2D) -> void:
+	in_attack_radius = true
+
+
+func _on_attack_range_body_exited(body: Node2D) -> void:
+	in_attack_radius = false
